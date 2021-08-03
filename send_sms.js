b79b9c572f3beae1fbe1f6cc2b90967390713ca3
Jose Rubio <ru310z@gmail.com>
2021-08-03 10:49:03 -0500

    Database linked. User is now saved at new database.

---
+++ send_sms.js
@@ -0,0 +1,16 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+require('dotenv').config();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+19382232238',
+     to: '+17737121439'
+   })
+  .then(message => console.log(message.sid));
--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -18,13 +18,13 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 // The only thing different from this and every other post we've seen
 // is that the password gets encrypted before being inserted
 router.post('/register', (req, res, next) => {
-  const username = req.body.username;
+  const email = req.body.username;
   const password = encryptLib.encryptPassword(req.body.password);
 
-  const queryText = `INSERT INTO "user" (username, password)
+  const queryText = `INSERT INTO "user" (email, password)
     VALUES ($1, $2) RETURNING id`;
   pool
-    .query(queryText, [username, password])
+    .query(queryText, [email, password])
     .then(() => res.sendStatus(201))
     .catch((err) => {
       console.log('User registration failed: ', err);
